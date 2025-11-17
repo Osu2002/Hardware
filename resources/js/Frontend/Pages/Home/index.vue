3e8fffd69e41e4b42bbb5c8e6eb212b2dc2a64c7
kushan-panda <kushan.20230023@iit.ac.lk>
2025-11-17 10:49:30 +0530

    changes

--- resources/js/Frontend/Pages/Home/index.vue
+++ resources/js/Frontend/Pages/Home/index.vue
@@ -1,7 +1,11 @@
 <template>
   <AppLayout>
+    <br><br>
     <div class="home-container">
       <ImageSlider />
+      <CategoriesBox :categories="categories" />
+      <br><br>
+      <SpecialOffers />
     </div>
 
    
@@ -9,15 +13,24 @@
 </template>
 
 <script>
-import AppLayout   from '@@/Layouts/AppLayout.vue';
+import AppLayout     from '@@/Layouts/AppLayout.vue';
+import CategoriesBox from './partials/Categoriesbox.vue';
+import ImageSlider   from './partials/ImageSlider.vue';
+import SpecialOffers from './partials/SpecialOffers.vue';
 
-import ImageSlider from './partials/ImageSlider.vue';
 
 export default {
-  components: { AppLayout, ImageSlider },
-  props: { site_url: Object },
+  components: { AppLayout, ImageSlider, CategoriesBox, SpecialOffers  },
+  props: {
+    site_url: Object,
+    categories: {
+      type: Array,
+      default: () => [],
+    },
+  },
 };
 </script>
+
 
 <style>
 .home-container{ max-width: 1320px; margin: 0 auto; padding: 0 12px; }
